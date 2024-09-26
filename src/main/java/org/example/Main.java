9419f3589504a4302cd011730f6e9f0875fd6d6a
rajbunsha <rajbunsha@gmail.com>
2024-09-26 15:12:00 +0530

    removed logger temporarily

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class);
+//    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
